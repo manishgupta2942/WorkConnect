716a62ba1209e5277bf296e3d47e6a68e65e995a
manishgupta2942 <139032999+manishgupta2942@users.noreply.github.com>
2024-09-22 23:10:32 +0530

    Update index.js phir se link diya

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: '*',
+    origin:'https://workconnect-1.onrender.com',
     
     credentials:true
 }
